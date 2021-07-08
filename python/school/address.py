130c808d615f4892592bdb7c37d3206c346ed47e
Ray-cmd <luca.ray@protonmail.ch>
2021-07-08 22:20:05 +0200

    updates

---
+++ python/school/address.py
@@ -0,0 +1,59 @@
+class Address():
+    def __init__(self, street, street_num, zip_code, city):
+        self.street = street
+        self.street_num = street_num
+        self.zip_code = zip_code
+        self.city = city
+
+    @property
+    def street(self):
+        return self.__street
+    @street.setter
+    def street(self, street):
+        if not isinstance(street, str):
+            raise ValueError("Street must be a string")
+        for char in street:
+            if char != " " and not char.isalpha():
+                raise ValueError("Street must be characters")
+        if len(street) < 3 or len(street) > 64:
+            raise ValueError("Street must be between 3 and 64 characters")
+        self.__street = street
+
+    @property
+    def street_num(self):
+        return self.__street_num
+    @street_num.setter
+    def street_num(self, street_num):
+        if not isinstance(street_num, int):
+            raise ValueError("Street number must be an integer")
+        if len(str(street_num)) > 2:
+            raise ValueError("Street number canont be longer than 2 digits")
+        if street_num < 1:
+            raise ValueError("Street number canont be lower than 1")
+        self.__street_num = street_num
+
+    @property
+    def zip_code(self):
+        return self.__zip_code
+    @zip_code.setter
+    def zip_code(self, zip_code):
+        if not isinstance(zip_code, int):
+            raise ValueError("Zip code must be an integer")
+        if len(str(zip_code)) != 4 :
+            raise ValueError("Swiss zip code are 4 digits long")
+        self.__zip_code = zip_code
+
+    @property
+    def city(self):
+        return self.__city
+    @city.setter
+    def city(self, city):
+        if not isinstance(city, str):
+            raise ValueError("City must be a string")
+        if len(city) < 3 or len(city) > 64:
+            raise ValueError("City must be between 3 and 64 characters")
+        self.__city = city
+
+    def __str__(self):
+        return f"{self.street} {self.street_num} {self.zip_code} {self.city}"
+
--- python/school/person.py
+++ python/school/person.py
@@ -1,18 +1,20 @@
+from address import Address
+
 import datetime
+import re
 
 class Person():
-    def __init__(self, last_name, first_name, birth_date, num_tel):
+    def __init__(self, last_name, first_name, birth_date, num_tel, email = None, address_infos = None):
         self.last_name = last_name
         self.first_name = first_name
         self.birth_date = birth_date
         self.num_tel = num_tel
-        self.email = None
-        self.address = None
+        self.email = email
+        self.address_infos = address_infos
       
     @property
     def last_name(self):
         return self.__last_name.capitalize()
-
     @last_name.setter
     def last_name(self, last_name):
         if not isinstance(last_name, str):
@@ -26,7 +28,6 @@ class Person():
     @property
     def first_name(self):
         return self.__first_name.capitalize()
-
     @first_name.setter
     def first_name(self, first_name):
         if not isinstance(first_name, str):
@@ -40,7 +41,6 @@ class Person():
     @property
     def birth_date(self):
         return datetime.datetime.fromtimestamp(self.__birth_date).strftime("%d.%m.%Y")
-
     @birth_date.setter
     def birth_date(self, birth_date):
         if not isinstance(birth_date, datetime.datetime) and not isinstance(birth_date, str):
@@ -54,7 +54,6 @@ class Person():
     @property
     def num_tel(self):
         return self.__num_tel
-
     @num_tel.setter
     def num_tel(self, num_tel):
         if not isinstance(num_tel, str):
@@ -67,21 +66,53 @@ class Person():
 
         self.__num_tel = num_tel
 
+    @property
+    def email(self):
+        return self.__email
+    @email.setter
+    def email(self, email):
+        if email is None:
+            self.__email = email
+            return
+        if not isinstance(email, str):
+            raise ValueError("Email must be a string")
+        if not re.search("^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$", email):
+            raise ValueError("Email is not valid")
+        self.__email = email
+    
+
+    @property
+    def address_infos(self):
+        return self.__address_infos
+    @address_infos.setter
+    def address_infos(self, address_infos):
+        if not isinstance(address_infos, Address) or address_infos != None:
+            print("pas une adresse")
+        self.__address_infos = address_infos
+
     def __str__(self):
-        return f"Last name : {self.last_name} First name : {self.first_name} Birth date : {self.birth_date} Telephon : {self.num_tel}"
+        return f"Last name : {self.last_name}\nFirst name : {self.first_name}\nBirth date : {self.birth_date}\nTelephon : {self.num_tel}\nEmail : {self.email}\nAddress : {self.address_infos}"
 
 def main():
     #date = "23.08.1992"
     date = datetime.datetime.strptime("22.08.1992", "%d.%m.%Y")
-
     tel = "0244455902"
+    email = "luca.ray@pm.me"
 
-    tmp = Person("Ray", "Luca", date, tel)
-    print(tmp)
-    tmp.last_name = "Dylan"
-    tmp.first_name = "Bob"
-    tmp.birth_date = "01.01.1990"
-    tmp.num_tel = "024 400 00 00"
-    print(tmp)
+    street = "Sous la Loge"
+    street_num = 5
+    zip_code = 1442
+    city = "Montagny-près-Yverdon"
+
+    address = Address(street, street_num, zip_code, city)
+    person = Person("Ray", "Luca", date, tel, email, address)
+    print(person)
+
+    person.address_infos.street_num = 22
+
+    print(person)
+
+    
+    
 
 main()
