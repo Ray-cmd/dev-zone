076ba60a3ce7aa739959db602c83d8b00db88aa5
Luca Ray <luca.ray@protonmail.ch>
2021-08-30 18:13:27 +0200

    update

--- web/JS/accordion/accordion.js
+++
@@ -1,8 +0,0 @@
-const btnAccordion = document.querySelectorAll(".accordion__header")
-
-for (let i = 0; i < btnAccordion.length; i++) {
-    btnAccordion[i].addEventListener("click", () => {
-        let panelAccordion = btnAccordion[i].nextSibling.nextSibling
-        panelAccordion.classList.toggle("accordion__panel--open")
-    })
-}
