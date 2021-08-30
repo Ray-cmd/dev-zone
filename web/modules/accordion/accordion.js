7ef562b2114225e4619751c19f37ef83b0267127
Luca Ray <luca.ray@protonmail.ch>
2021-08-30 20:50:25 +0200

    update

--- web/modules/accordion/accordion.js
+++ web/modules/accordion/accordion.js
@@ -1,8 +1,13 @@
-const btnAccordion = document.querySelectorAll(".accordion__header")
+const btnAccordion = document.querySelectorAll(".accordion")
 
-for (let i = 0; i < btnAccordion.length; i++) {
-    btnAccordion[i].addEventListener("click", () => {
-        let panelAccordion = btnAccordion[i].nextSibling.nextSibling
-        panelAccordion.classList.toggle("accordion__panel--open")
-    })
+for ( let i = 0; i < btnAccordion .length; i++) {
+    btnAccordion [i].addEventListener("click", function() {
+        this.classList.toggle("accordion--active");
+        let panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
 }
