88d56bfa0cc670dba95bc95d5523e6d98cdd9cde
Ray-cmd <luca.ray@protonmail.ch>
2022-03-16 17:48:27 +0100

    ménage

---
+++ Dev/python/modules/pwdGen/pwdGen.py
@@ -0,0 +1,116 @@
+import string
+import random
+
+class PwdGen:
+    __LOWERCASES = string.ascii_lowercase
+    __UPPERCASES = string.ascii_uppercase
+    __NUMBERS = string.digits
+    __SYMBOLS = string.punctuation
+
+    __nbr_char = 12
+    __lowercase = True
+    __uppercase = True
+    __number = True
+    __symbol = False
+    __password = ""
+
+    def __init__(self, nbr_char = None, lowercase = None, uppercase = None, number = None, symbol = None, password = None):
+        if nbr_char is not None:
+            self.__nbr_char = nbr_char
+        if lowercase is not None:
+            self.__lowercase = lowercase
+        if uppercase is not None:
+            self.__uppercase = uppercase
+        if number is not None:
+            self.__number = number
+        if symbol is not None:
+            self.__symbol = symbol
+        if password is None:
+            self.generate()
+        else:
+            self.__password = password
+
+    @property
+    def nbr_char(self):
+        return self.__nbr_char
+
+    @nbr_char.setter
+    def nbr_char(self, nbr_char):
+        if not isinstance(nbr_char, int):
+            raise TypeError("nbr_char must be an integer")
+        if nbr_char < 6 and nbr_char > 128:
+            raise ValueError("nbr_char must be between 6 and 128 characters")
+        self.__nbr_char = nbr_char
+
+    @property
+    def lowercase(self):
+        return self.__lowercase
+
+    @lowercase.setter
+    def lowercase(self, lowercase):
+        if not isinstance(lowercase, bool):
+            raise TypeError("lowercase must be a booleen")
+        self.__lowercase = lowercase
+
+    @property
+    def uppercase(self):
+        return self.__uppercase
+
+    @uppercase.setter
+    def uppercase(self, uppercase):
+        if not isinstance(uppercase, bool):
+            raise TypeError("uppercase must be a booleen")
+        self.__uppercase = uppercase
+
+    @property
+    def number(self):
+        return self.__number
+
+    @number.setter
+    def number(self, number):
+        if not isinstance(number, bool):
+            raise TypeError("number must be a booleen")
+        self.__number = number
+
+    @property
+    def symbol(self):
+        return self.__symbol
+
+    @symbol.setter
+    def symbol(self, symbol):
+        if not isinstance(symbol, bool):
+            raise TypeError("symbol must be a booleen")
+        self.__symbol = symbol
+        
+    def __make_sample(self):
+        sample = ""
+        if self.lowercase:
+            sample += self.__LOWERCASES
+        if self.uppercase:
+            sample += self.__UPPERCASES
+        if self.number:
+            sample += self.__NUMBERS
+        if self.symbol:
+            sample += self.__SYMBOLS
+        return sample
+
+    def generate(self):
+        sample = self.__make_sample()
+        password = ""
+        for _ in range(self.nbr_char):
+            password += random.choice(sample)
+        self.__password = password
+        
+    def __str__(self):
+        return str(self.__password)
+
+    def __repr__(self):
+        return str(self.__password)
+
+def main():
+    PwdGen.symbol = True
+    for _ in range(5):
+        print(PwdGen())
+
+if __name__ == '__main__':
+    main()
---
+++ Dev/python/school/person.py
@@ -0,0 +1,106 @@
+from address import Address
+
+import datetime
+import re
+
+class Person():
+    def __init__(self, last_name, first_name, birth_date, num_tel, email = None, address_infos = None):
+        self.last_name = last_name
+        self.first_name = first_name
+        self.birth_date = birth_date
+        self.num_tel = num_tel
+        self.email = email
+        self.address_infos = address_infos
+      
+    @property
+    def last_name(self):
+        return self.__last_name.capitalize()
+    @last_name.setter
+    def last_name(self, last_name):
+        if not isinstance(last_name, str):
+            raise TypeError("Last name must be a string")
+        if len(last_name) < 2:
+            raise ValueError("Last name must be at least 2 characters")
+        if len(last_name) > 64:
+            raise ValueError("Last name must not exceed 64 characters")
+        self.__last_name = last_name.lower()
+   
+    @property
+    def first_name(self):
+        return self.__first_name.capitalize()
+    @first_name.setter
+    def first_name(self, first_name):
+        if not isinstance(first_name, str):
+            raise TypeError("First name must be a string")
+        if len(first_name) < 2:
+            raise ValueError("First name must be at lest 2 characters")
+        if len(first_name) > 32:
+            raise ValueError("First name must not exceed 32 characters")
+        self.__first_name = first_name.lower()
+
+    @property
+    def birth_date(self):
+        return datetime.datetime.fromtimestamp(self.__birth_date).strftime("%d.%m.%Y")
+    @birth_date.setter
+    def birth_date(self, birth_date):
+        if not isinstance(birth_date, datetime.datetime) and not isinstance(birth_date, str):
+            raise ValueError("Birth date must be a string or a datetime.datetime object")
+        if isinstance(birth_date, str):
+            birth_date = datetime.datetime.strptime(birth_date, "%d.%m.%Y")
+
+        timestamp = birth_date.timestamp()
+        self.__birth_date = timestamp
+
+    @property
+    def num_tel(self):
+        return self.__num_tel
+    @num_tel.setter
+    def num_tel(self, num_tel):
+        if not isinstance(num_tel, str):
+            raise ValueError("Telephone number must be a string")
+        num_tel = num_tel.replace(" ", "")
+        if not num_tel.isdigit():
+            raise ValueError("Telephone number must be made of numbers")
+        if not len(num_tel) == 10:
+            raise ValueError("Telephone number must be a 10 numbers swiss number") 
+
+        self.__num_tel = num_tel
+
+    @property
+    def email(self):
+        return self.__email
+    @email.setter
+    def email(self, email):
+        if email is None:
+            self.__email = email
+            return
+        if not isinstance(email, str):
+            raise ValueError("Email must be a string")
+        if not re.search("^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$", email):
+            raise ValueError("Email is not valid")
+        self.__email = email
+
+    @property
+    def address_infos(self):
+        return self.__address_infos
+    @address_infos.setter
+    def address_infos(self, address_infos):
+        if address_infos is None:
+            self.__address_infos = None
+            return
+        if isinstance(address_infos, list) or isinstance(address_infos, tuple):
+            address_infos = Address(address_infos[0], address_infos[1], address_infos[2], address_infos[3])
+        if not isinstance(address_infos, Address):
+            raise ValueError("Address should be a list, a tuple or a Address")
+
+
+        self.__address_infos = address_infos
+
+    def __str__(self):
+        return f"Last name : {self.last_name}\nFirst name : {self.first_name}\nBirth date : {self.birth_date}\nTelephon : {self.num_tel}\nEmail : {self.email}\nAddress : {self.address_infos}"
+
+def main():
+    pass
+
+main()
+
