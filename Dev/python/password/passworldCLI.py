9b800be8efe7640a486f2bf41bf4a8960470f0ac
Ray-cmd <luca.ray@protonmail.ch>
2022-06-30 22:07:32 +0200

    rework code flow and add rich text

--- Dev/python/password/passworldCLI.py
+++ Dev/python/password/passworldCLI.py
@@ -1,9 +1,34 @@
+"""
+Password Generator
+    The software is maid to generate password,
+    You can generate password in bulk and export them in a txt file
+
+TODO
+    - Add posibility to input a path with the --export flag
+        This feature will allow to chose in wich directory 
+        the file will be created. It will also allow to chose
+        the name of the file
+        EXEMPLE : passworldCLI.py -s -l32 -n100 --export ~/Documents/password  
+"""
 import argparse
 from datetime import datetime
 import pyperclip
+from rich.console import Console
 
 from libs.password import Password as pwd
 
+def write_file(password):
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = f"{time}-password.txt"
+    with open(filename, "w") as file:
+        if type(password) == str:
+            file.write(str(password))
+        else :
+            for el in password:
+                file.write(str(el))
+                file.write("\n")
+    console.print(f"[bold][+][/] The file [bold underline green]{filename}[/] was generate", style="green")
+    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate a password")
     parser.add_argument(
@@ -40,24 +65,23 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    passwords = []
-    for _ in range(args.num):
-        password = pwd(length=args.length, has_digits=args.digits, has_specials=args.specials)
+    console = Console()
+    print("")
+    if args.num == 1:
+        gen_pwd = str(pwd(length=args.length, has_digits=args.digits, has_specials=args.specials))
         if not args.export:
-            print(password)
-        passwords.append(password)
+            print(gen_pwd)
+            print("")
+        pyperclip.copy(gen_pwd)
+        console.print("[bold][+][/] The password was copied to the clipboard", style="green")
+    else:
+        gen_pwd = []
+        for _ in range(args.num):
+            password = pwd(length=args.length, has_digits=args.digits, has_specials=args.specials)
+            gen_pwd.append(password)
+            if not args.export:
+                print(password)
 
     if args.export:
-        if args.num == 1:
-            password = str(passwords[0])
-            pyperclip.copy(password)
-            print(f"The password {password} was copied on the clipboard")
-
-        else:
-            time = datetime.now().strftime("%Y%m%d%H%M%S")
-            filename = f"{time}-password.txt"
-            with open(filename, "w") as file:
-                for password in passwords:
-                    file.write(str(password))
-                    file.write("\n")
-            print(f"The file {filename} was generate with {args.num} passwords")
+        write_file(gen_pwd)
+    print("")
